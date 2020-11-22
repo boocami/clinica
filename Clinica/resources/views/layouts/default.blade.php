3e93d75fd6b7abee922b76648738f51329cd6db4
boocami <56263588+boocami@users.noreply.github.com>
2020-11-22 00:03:02 -0300

    Mi primmer commit, proyecto de laravel

---
+++ Clinica/resources/views/layouts/default.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=9" />
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9" />
+
+    <title>Centro Medico Howick</title>
+@section('script')
+
+@show
+
+</head>
+
+<body style="padding-top: 0px !important">
+
+        @include('layouts.navTop')
+
+        @section('content')
+
+
+        @show
+    </div>
+
+    <footer>
+    </footer>
+    @section('loadScript')
+
+    @show
+</body>
+
+</html>
